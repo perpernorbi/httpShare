cb80188142a829ee396613b6995321ec24783d22
Koszó Norbert <k.norbi@gmail.com>
2015-06-14 15:53:03 +0200

    shUnit working - no test added yet

---
+++ test/httpShare_test_options.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+. ./httpShare_test_helpers
+
+#------------------------------------------------------------------------------
+# suite tests
+#
+
+testItIsWorking()
+{
+    assertEquals 1 1
+}
